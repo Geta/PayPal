46a529ab6f76a0ab43ee55c8ba71f239b64670bc
Rui Vaz <dig.int.sys@gmail.com>
2018-03-16 10:13:47 +0100

    adding source code and demo

---
+++ src/Geta.Commerce.Payments.PayPal/Extensions/IOrderGroupBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Commerce.Order;
+using EPiServer.Commerce.Order.Internal;
+
+namespace Geta.Commerce.Payments.PayPal.Extensions
+{
+    public static class IOrderGroupBuilderExtensions
+    {
+        public static IPayment CreatePayPalPayment(this IOrderGroupBuilder orderGroupBuilder)
+        {
+            if (orderGroupBuilder is SerializableCartBuilder)
+            {
+                return new SerializablePayPalPayment();
+            }
+
+            return new PayPalPayment();
+        }
+    }
+}
